c6b80e2c1a330d46621e9d0ac5fc93b7f5bc2ebe
Issac <issac725725@gmail.com>
2022-01-28 14:11:08 +1030

    Remove circulate function call, add basic controller and view component - html using thymeleaf

--- src/main/java/guru/springframework/spring5webapp/controller/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controller/BookController.java
@@ -1,4 +1,27 @@
-package guru.springframework.spring5webapp.controller;/**
+package guru.springframework.spring5webapp.controller;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
  * Created by @author issaclee725 on 28/01/2022
- */public class BookController {
+ */
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books/list";
+    }
 }
